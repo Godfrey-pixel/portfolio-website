7097ab19cc8ce6c695cebd2e96b5a706d1fcddf8
Godfrey <thabisothabisogs@gmail.com>
2026-06-22 12:27:16 +0200

    Improved UI

--- core/views.py
+++ core/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+from projects.models import Project
 
 
 def home(request):
-    return render(request, 'home.html')
+    projects = Project.objects.all()
+    return render(request, 'home.html', {'projects': projects})
 
 def about(request):
     return render(request, 'about.html')
@@ -11,4 +15,26 @@ def services(request):
     return render(request, 'services.html')
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        full_message = f"""
+        Message from: {name}
+        Email: {email}
+
+        Message:
+        {message}   
+        """
+
+        send_mail(
+            subject='New Contact Form Submission',
+            message=full_message,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[settings.EMAIL_HOST_USER],
+            fail_silently=False,
+        )
+
+        return render(request, 'contact.html', {"success": True})
     return render(request, 'contact.html')
