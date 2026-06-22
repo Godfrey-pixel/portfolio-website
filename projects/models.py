e518253a60b38a03246280c2182e01e4e8760fc9
Godfrey <thabisothabisogs@gmail.com>
2026-06-22 11:55:24 +0200

    Added Projects section

--- projects/models.py
+++ projects/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Project(models.Model):
@@ -8,7 +9,25 @@ class Project(models.Model):
     github_link = models.URLField(blank=True, null=True)
     live_link = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    technologies = models.CharField(max_length=300, blank=True)
+    slug = models.SlugField(unique=True, max_length=200, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.title)
+            slug = base_slug
+            counter = 1
+
+            while Project.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+
+            self.slug = slug
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
-
+    
+    class Meta:
+        ordering = ['-created_at']
